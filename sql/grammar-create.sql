7066f36379f4c2a465b72fca47f4aab79502665d
C.J. Adams-Collier <cjac@colliertech.org>
2015-12-27 14:02:07 -0800

    moved create database and create user out of grammar-create.sql ; fixed some typos ; added NOT NULL constraint ; renamed repeat to rpt to avoid namespace conflicts

--- sql/grammar-create.sql
+++ sql/grammar-create.sql
@@ -1,22 +1,21 @@
 /* Copyright 2015 Collier Technologies LLC */
 
-CREATE DATABASE IF NOT EXISTS grammar;
+use grammar;
 
-CREATE USER 'grammaradm'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL ON grammar.* TO 'grammaradm'@'localhost';
+source syntactic_category/create-table.sql;
 
 CREATE TABLE IF NOT EXISTS lexeme (
   id     INT AUTO_INCREMENT PRIMARY KEY,
   word   VARCHAR(64),
-  pos_id INT,
-  CONSTRAINT fk_lexeme_lexcat (pos_id) REFERENCES lexical_category(id)
+  pos_id INT NOT NULL,
+  CONSTRAINT fk_lexeme_lexcat FOREIGN KEY (pos_id) REFERENCES _lexical_category(id)
 ) ENGINE=INNODB, COMMENT='Words available for use and their associated properties';
 
 CREATE TABLE IF NOT EXISTS phrase_structure_rule (
   id        INT AUTO_INCREMENT PRIMARY KEY,
   target_id INT NOT NULL,
 
-  CONSTRAINT fk_phstrule_pcat FOREIGN KEY (target_id) REFERENCES phrasal_category(id)
+  CONSTRAINT fk_phstrule_pcat FOREIGN KEY (target_id) REFERENCES _phrasal_category(id)
 ) ENGINE=INNODB, COMMENT='Rules indicating valid grammatical constructs';
 
 CREATE TABLE IF NOT EXISTS rule_node (
@@ -25,10 +24,10 @@ CREATE TABLE IF NOT EXISTS rule_node (
   rule_id   INT NOT NULL,
   position  INT NOT NULL,
 
-  scat_id   INT DEFAULT NULL,
+  scat_id   INT NOT NULL,
 
-  optional  BIT(1) NOT NULL DEFAULT b'0',
-  repeat    BIT(1) NOT NULL DEFAULT b'0',
+  optional  BIT NOT NULL DEFAULT b'0',
+  rpt       BIT NOT NULL DEFAULT b'0', /* short for repeat, but that is a keyword */
 
   CONSTRAINT uniq_rule_position UNIQUE (rule_id,position),
   CONSTRAINT fk_rcp_rule FOREIGN KEY (rule_id) REFERENCES phrase_structure_rule(id),

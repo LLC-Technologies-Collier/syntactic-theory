da8c2f11054ab20b195c536721581c9da7d5d4bb
C.J. Adams-Collier <cjac@colliertech.org>
2015-12-28 14:29:09 -0800

    added access to syntactic_category table

--- sql/create-user.sql
+++ sql/create-user.sql
@@ -5,6 +5,7 @@ GRANT ALL ON grammar.* TO 'grammaradm'@'localhost';
 
 CREATE USER 'grammaruser'@'localhost' IDENTIFIED BY 'nunya';
 GRANT SELECT ON grammar.lexeme                TO 'grammaruser'@'localhost';
+GRANT SELECT ON grammar.syntactic_category    TO 'grammaruser'@'localhost';
 GRANT SELECT ON grammar.lexical_category      TO 'grammaruser'@'localhost';
 GRANT SELECT ON grammar.phrasal_category      TO 'grammaruser'@'localhost';
 GRANT SELECT ON grammar.phrase_structure_rule TO 'grammaruser'@'localhost';

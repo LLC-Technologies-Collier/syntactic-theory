2212e7e8622f7e8c5f4922d7946dc220e90317b6
C.J. Adams-Collier <cjac@colliertech.org>
2015-12-28 13:02:37 -0800

    created read-only user and granted select for necessary tables and views

--- sql/create-user.sql
+++ sql/create-user.sql
@@ -2,3 +2,10 @@
 
 CREATE USER 'grammaradm'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL ON grammar.* TO 'grammaradm'@'localhost';
+
+CREATE USER 'grammaruser'@'localhost' IDENTIFIED BY 'nunya';
+GRANT SELECT ON grammar.lexeme                TO 'grammaruser'@'localhost';
+GRANT SELECT ON grammar.lexical_category      TO 'grammaruser'@'localhost';
+GRANT SELECT ON grammar.phrasal_category      TO 'grammaruser'@'localhost';
+GRANT SELECT ON grammar.phrase_structure_rule TO 'grammaruser'@'localhost';
+GRANT SELECT ON grammar.rule_node             TO 'grammaruser'@'localhost';

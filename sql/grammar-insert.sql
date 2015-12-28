74038d1dc28559378bc54471a8a120021d1f0269
C.J. Adams-Collier <cjac@colliertech.org>
2015-12-27 17:42:51 -0800

    inserted rows for words from page 27

--- sql/grammar-insert.sql
+++ sql/grammar-insert.sql
@@ -5,6 +5,7 @@ DELETE FROM phrasal_category_;
 DELETE FROM _lexical_category;
 DELETE FROM lexical_category_;
 DELETE FROM syntactic_category;
+DELETE FROM lexeme;
 
 INSERT INTO lexical_category_ ( label, longname )
                        VALUES ( 'D',   'Determiner' ),
@@ -21,3 +22,21 @@ VALUES ( 'Adjective Phrase',     'AP',   ( SELECT catid_by_label('A')   ) ),
        ( 'Noun Phrase',          'NP',   ( SELECT catid_by_label('N')   ) ),
        ( 'Verb Phrase',          'VP',   ( SELECT catid_by_label('V')   ) ),
        ( 'Prepositional Phrase', 'PP',   ( SELECT catid_by_label('P')   ) );
+
+
+INSERT INTO lexeme ( word, pos_id )
+            VALUES ( 'the',     ( SELECT catid_by_label('D') ) ),
+	           ( 'some',    ( SELECT catid_by_label('D') ) ),
+		   ( 'big',     ( SELECT catid_by_label('A') ) ),
+		   ( 'brown',   ( SELECT catid_by_label('A') ) ),
+		   ( 'old',     ( SELECT catid_by_label('A') ) ),
+		   ( 'birds',   ( SELECT catid_by_label('N') ) ),
+		   ( 'fleas',   ( SELECT catid_by_label('N') ) ),
+		   ( 'dog',     ( SELECT catid_by_label('N') ) ),
+		   ( 'hunter',  ( SELECT catid_by_label('N') ) ),
+		   ( 'attack',  ( SELECT catid_by_label('V') ) ),
+		   ( 'ate',     ( SELECT catid_by_label('V') ) ),
+		   ( 'watched', ( SELECT catid_by_label('V') ) ),
+		   ( 'for',     ( SELECT catid_by_label('P') ) ),
+		   ( 'beside',  ( SELECT catid_by_label('P') ) ),
+		   ( 'with',    ( SELECT catid_by_label('P') ) );

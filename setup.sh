6e931e15cff853416ed942cbb8d3f85ed3713605
C.J. Adams-Collier <cjac@colliertech.org>
2015-12-30 13:12:04 -0800

    added a script to automate instantiation of the database

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Copyright 2015 Collier Technologies LLC
+
+BASEDIR=$CWD
+
+cd sql
+mysql -u root --password=password < create-database.sql
+mysql -u root --password=password < create-user.sql
+mysql -u grammaradm --password=password grammar < grammar-create.sql
+mysql -u grammaradm --password=password grammar < grammar-insert.sql

c4a4e2bd729c9756f51ab29d8f2543c35112e837
C.J. Adams-Collier <cjac@colliertech.org>
2016-01-21 17:30:45 -0800

    using SQL variables instead of constantly executing those SELECT statements

--- sql/grammar-insert.sql
+++ sql/grammar-insert.sql
@@ -17,73 +17,88 @@ INSERT INTO lexical_category_ ( label, longname )
                    ( 'V',   'Verb' ),
                    ( 'P',   'Preposition' );
 
+SET @determiner_catid  = ( SELECT catid_by_label('D') );
+SET @adjective_catid   = ( SELECT catid_by_label('A') );
+SET @adverb_catid      = ( SELECT catid_by_label('Adv') );
+SET @noun_catid        = ( SELECT catid_by_label('N') );
+SET @verb_catid        = ( SELECT catid_by_label('V') );
+SET @preposition_catid = ( SELECT catid_by_label('P') );
+
 INSERT INTO phrasal_category_
-       ( longname, label, head_cat_id )
-VALUES ( 'Adjective Phrase',     'AP',   ( SELECT catid_by_label('A')   ) ),
-       ( 'Adverb Phrase',        'AdvP', ( SELECT catid_by_label('Adv') ) ),
-       ( 'Noun Phrase',          'NP',   ( SELECT catid_by_label('N')   ) ),
-       ( 'Verb Phrase',          'VP',   ( SELECT catid_by_label('V')   ) ),
-       ( 'Prepositional Phrase', 'PP',   ( SELECT catid_by_label('P')   ) ),
-       ( 'Sentence',             'S',    ( SELECT catid_by_label('N')   ) );
+       ( longname,               label,  head_cat_id )
+VALUES ( 'Determiner Phrase',    'DP',   @determiner_catid ),
+       ( 'Adjective Phrase',     'AP',   @adjective_catid ),
+       ( 'Adverb Phrase',        'AdvP', @adverb_catid ),
+       ( 'Noun Phrase',          'NP',   @noun_catid ),
+       ( 'Verb Phrase',          'VP',   @verb_catid ),
+       ( 'Prepositional Phrase', 'PP',   @preposition_catid ),
+       ( 'Sentence',             'S',    ( SELECT catid_by_label('N')   ) ); /* probably wrong... */
+
+SET @AP_catid   = ( SELECT catid_by_label('AP') );
+SET @AdvP_catid = ( SELECT catid_by_label('AdvP') );
+SET @NP_catid   = ( SELECT catid_by_label('NP') );
+SET @VP_catid   = ( SELECT catid_by_label('VP') );
+SET @PP_catid   = ( SELECT catid_by_label('PP') );
+SET @S_catid    = ( SELECT catid_by_label('S') );
 
 
 INSERT INTO lexeme ( word, cat_id )
-VALUES ( 'the',        ( SELECT catid_by_label('D') ) ),
-       ( 'a',          ( SELECT catid_by_label('D') ) ),
-       ( 'some',       ( SELECT catid_by_label('D') ) ),
-       ( 'big',        ( SELECT catid_by_label('A') ) ),
-       ( 'brown',      ( SELECT catid_by_label('A') ) ),
-       ( 'old',        ( SELECT catid_by_label('A') ) ),
-       ( 'birds',      ( SELECT catid_by_label('N') ) ),
-       ( 'fleas',      ( SELECT catid_by_label('N') ) ),
-       ( 'dog',        ( SELECT catid_by_label('N') ) ),
-       ( 'hunter',     ( SELECT catid_by_label('N') ) ),
-       ( 'I',          ( SELECT catid_by_label('N') ) ),
-       ( 'astronomer', ( SELECT catid_by_label('N') ) ),
-       ( 'telescope',  ( SELECT catid_by_label('N') ) ),
-       ( 'attack',     ( SELECT catid_by_label('V') ) ),
-       ( 'saw',        ( SELECT catid_by_label('V') ) ),
-       ( 'ate',        ( SELECT catid_by_label('V') ) ),
-       ( 'watched',    ( SELECT catid_by_label('V') ) ),
-       ( 'for',        ( SELECT catid_by_label('P') ) ),
-       ( 'beside',     ( SELECT catid_by_label('P') ) ),
-       ( 'with',       ( SELECT catid_by_label('P') ) );
+VALUES ( 'the',        @determiner_catid ),
+       ( 'a',          @determiner_catid ),
+       ( 'some',       @determiner_catid ),
+       ( 'big',        @adjective_catid ),
+       ( 'brown',      @adjective_catid ),
+       ( 'old',        @adjective_catid ),
+       ( 'birds',      @noun_catid ),
+       ( 'fleas',      @noun_catid ),
+       ( 'dog',        @noun_catid ),
+       ( 'hunter',     @noun_catid ),
+       ( 'I',          @noun_catid ),
+       ( 'astronomer', @noun_catid ),
+       ( 'telescope',  @noun_catid ),
+       ( 'attack',     @verb_catid ),
+       ( 'saw',        @verb_catid ),
+       ( 'ate',        @verb_catid ),
+       ( 'watched',    @verb_catid ),
+       ( 'for',        @preposition_catid ),
+       ( 'beside',     @preposition_catid ),
+       ( 'with',       @preposition_catid );
 
 INSERT INTO phrase_structure_rule ( target_id, node_count )
-                           VALUES ( ( SELECT catid_by_label('PP') ), 2 );
+                           VALUES ( @PP_catid, 2 );
 
 SET @this_rule_id = LAST_INSERT_ID();
 
-INSERT INTO rule_node ( rule_id,       position, cat_id,                          optional, rpt )
-               VALUES ( @this_rule_id, '1',      ( SELECT catid_by_label('P')  ), 0,        0 ),
-                      ( @this_rule_id, '2',      ( SELECT catid_by_label('NP') ), 0,        0 );
+INSERT INTO rule_node ( rule_id,       position, cat_id,             optional, rpt )
+               VALUES ( @this_rule_id, '1',      @preposition_catid, 0,        0 ),
+                      ( @this_rule_id, '2',      @NP_catid,          0,        0 );
 
 INSERT INTO phrase_structure_rule ( target_id, node_count )
-                           VALUES ( ( SELECT catid_by_label('VP') ), 3 );
+                           VALUES ( @VP_catid, 3 );
 
 SET @this_rule_id = LAST_INSERT_ID();
 
-INSERT INTO rule_node ( rule_id,       position, cat_id,                          optional, rpt )
-               VALUES ( @this_rule_id, '1',      ( SELECT catid_by_label('V')  ), 0,        0 ),
-                      ( @this_rule_id, '2',      ( SELECT catid_by_label('NP') ), 1,        0 ),
-                      ( @this_rule_id, '3',      ( SELECT catid_by_label('PP') ), 1,        0 );
+INSERT INTO rule_node ( rule_id,       position, cat_id,      optional, rpt )
+               VALUES ( @this_rule_id, '1',      @verb_catid, 0,        0 ),
+                      ( @this_rule_id, '2',      @NP_catid,   1,        0 ),
+                      ( @this_rule_id, '3',      @PP_catid,   1,        0 );
 
 INSERT INTO phrase_structure_rule ( target_id, node_count )
-                           VALUES ( ( SELECT catid_by_label('NP') ), 4 );
+                           VALUES ( @NP_catid, 4 );
 
 SET @this_rule_id = LAST_INSERT_ID();
 
-INSERT INTO rule_node ( rule_id,       position, cat_id,                          optional, rpt )
-               VALUES ( @this_rule_id, '1',      ( SELECT catid_by_label('D')  ), 1,        0 ),
-                      ( @this_rule_id, '2',      ( SELECT catid_by_label('A')  ), 1,        1 ),
-                      ( @this_rule_id, '3',      ( SELECT catid_by_label('N')  ), 0,        0 ),
-                      ( @this_rule_id, '4',      ( SELECT catid_by_label('PP') ), 1,        1 );
+INSERT INTO rule_node ( rule_id,       position, cat_id,            optional, rpt )
+               VALUES ( @this_rule_id, '1',      @determiner_catid, 1,        0 ),
+                      ( @this_rule_id, '2',      @adjective_catid,  1,        1 ),
+                      ( @this_rule_id, '3',      @noun_catid,       0,        0 ),
+                      ( @this_rule_id, '4',      @PP_catid,         1,        1 );
 
 INSERT INTO phrase_structure_rule ( target_id, node_count )
                            VALUES ( ( SELECT catid_by_label('S') ), 2 );
 
 SET @this_rule_id = LAST_INSERT_ID();
 
-INSERT INTO rule_node ( rule_id,       position, cat_id,                          optional, rpt )
-               VALUES ( @this_rule_id, '1',      ( SELECT catid_by_label('NP') ), 0,        0 ),
-                      ( @this_rule_id, '2',      ( SELECT catid_by_label('VP') ), 0,        0 );
+INSERT INTO rule_node ( rule_id,       position, cat_id,    optional, rpt )
+               VALUES ( @this_rule_id, '1',      @NP_catid, 0,        0 ),
+                      ( @this_rule_id, '2',      @VP_catid, 0,        0 );

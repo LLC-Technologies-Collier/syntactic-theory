c9b2b870a7490665b4fea3265ac8a586691d33d0
C.J. Adams-Collier <cjac@colliertech.org>
2016-01-24 18:36:31 -0800

    re-factored Grammer.pm to make use of the new Symbol classes

--- Syntactic-Practice/lib/Syntactic/Practice/Grammar.pm
+++ Syntactic-Practice/lib/Syntactic/Practice/Grammar.pm
@@ -1,12 +1,41 @@
 package Syntactic::Practice::Grammar;
 
 use Syntactic::Practice::Util;
+use Syntactic::Practice::Grammar::Symbol::Lexical;
+use Syntactic::Practice::Grammar::Symbol::Phrasal;
 
 use Moose;
 
 has 'locale' => ( is      => 'ro',
                   isa     => 'Str',
                   default => 'en_US.UTF-8' );
+
+sub _lookup_rule {
+  my ( $self, $label ) = @_;
+
+  my $cond = { 'LOWER(target.label)' => { 'LIKE' => lc( $label ) } };
+  my $rs = $self->{rule_rs}->search( $cond );
+
+  my @rule;
+  while ( my $r = $rs->next() ) {
+    my $rule = { identifier => $r->target->label,
+                 symbols    => [] };
+    my $nodes = $r->rule_nodes;
+    while ( my $n = $nodes->next ) {
+      my $class =
+        'Syntactic::Practice::Grammar::Symbol::' . ucfirst $n->cat->ctype;
+      my $symbol = $class->new( label    => $n->cat->label,
+                                name     => $n->cat->longname,
+                                optional => $n->optional,
+                                repeat   => $n->rpt, );
+      $rule->{symbols}->[ $n->position - 1 ] = $node;
+    }
+    push( @rule, Syntactic::Practice::Grammar::Rule->new( $rule ) );
+  }
+  return \@rule;
+}
+
+my %rule;
 
 sub rule {
   my ( $self, @opt ) = @_;
@@ -20,39 +49,23 @@ sub rule {
 
   my $label = $opt->{label};
 
-  my $cond = { 'LOWER(target.label)' => { 'LIKE' => lc( $label ) } };
-  my $rs = $self->{rule_rs}->search( $cond );
+  $rule{$label} =
+    exists $rule{$label}
+    ? $rule{$label}
+    : $self->_lookup_rule( $label );
 
-  my @rule;
-  while ( my $r = $rs->next() ) {
-    my $rule = { label => $r->target->label,
-                 id    => $r->id,
-                 node  => [] };
-    my $nodes = $r->rule_nodes;
-    while ( my $n = $nodes->next ) {
-      my $bnf = $n->cat->label;
-      $bnf = ( $n->optional ? "[$bnf]" : "<$bnf>" );
-      $bnf .= '+' if $n->rpt;
-      my $node = { label    => $n->cat->label,
-                   name     => $n->cat->longname,
-                   cat_type    => $n->cat->ctype,
-                   bnf      => $bnf,
-                   optional => $n->optional,
-                   repeat   => $n->rpt, };
-      $rule->{node}->[ $n->position - 1 ] = $node;
+  return @{
+    $rule{$label}
+      if wantarray();
+      return $rule{$label};
     }
-    push( @rule, $rule );
-  }
-  return @rule if wantarray();
-  return \@rule;
-}
 
-around 'new' => sub {
-  my ( $orig, $self, @arg ) = @_;
+    around 'new' => sub {
+    my ( $orig, $self, @arg ) = @_;
 
-  my $schema = Syntactic::Practice::Util->new()->get_schema();
+    my $schema = Syntactic::Practice::Util->get_schema();
 
-  my $rule_rs = $schema->resultset( 'PhraseStructureRule' )->search(
+    my $rule_rs = $schema->resultset( 'PhraseStructureRule' )->search(
                                        {},
                                        {
                                          prefetch => [
@@ -60,12 +73,12 @@ around 'new' => sub {
                                          ]
                                        } );
 
-  my $obj = $self->$orig( @arg );
-  $obj->{schema}  = $schema;
-  $obj->{rule_rs} = $rule_rs;
+    my $obj = $self->$orig( @arg );
+    $obj->{schema}  = $schema;
+    $obj->{rule_rs} = $rule_rs;
 
-  return $obj;
-};
+    return $obj;
+    };
 
-no Moose;
-__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+  no Moose;
+  __PACKAGE__->meta->make_immutable( inline_constructor => 0 );

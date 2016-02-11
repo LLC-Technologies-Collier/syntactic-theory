a4035ac8f401cd75f1fdb3c826997851f982cbc1
C.J. Collier <cjcollier@linuxfoundation.org>
2016-02-11 02:32:01 -0800

    fleshed out token classes

--- Syntactic-Practice/lib/Syntactic/Practice/Lexer/Token.pm
+++ Syntactic-Practice/lib/Syntactic/Practice/Lexer/Token.pm
@@ -1,10 +1,8 @@
-package Syntactic::Practice::Lexer::Token;
-
-use strict;
+package Syntactic::Practice::Grammar::Token;
 
 =head1 NAME
 
-Syntactic::Practice::Lexer::Token - elements of lexical analysis
+Syntactic::Practice::Grammar::Token - An atomic unit of grammar
 
 =head1 VERSION
 
@@ -16,6 +14,84 @@ our $VERSION = '0.01';
 
 use Moose;
 use namespace::autoclean;
+use MooseX::Method::Signatures;
 
-__PACKAGE__->meta->make_immutable;
+has set => ( is       => 'ro',
+             isa      => 'TokenSet',
+             required => 1, );
+
+has tree => ( is       => 'ro',
+              isa      => 'Tree',
+              required => 1 );
+
+has next => ( is      => 'rw',
+              isa     => 'Maybe[Tree]',
+              lazy    => 1,
+              build   => '_build_next',
+              trigger => \&_set_next, );
+
+has prev => ( is      => 'rw',
+              isa     => 'Maybe[Tree]',
+              lazy    => 1,
+              build   => '_build_prev',
+              trigger => \&_set_prev, );
+
+sub copy {
+  my ( $self, %attr ) = @_;
+  $attr{set} //= Syntactic::Practice::Grammar::TokenSet->new();
+  %attr = ( %$self, %attr );
+  my $copy = $self->new( %attr );
+}
+
+method position () {
+  for ( my $i = 0; $i < $self->set->count; $i++ ) {
+    return $i if $self->set->tokens[$i]->cmp( $self ) == 0;
+  }
+  my $msg = 'Did not find self in TokenSet';
+  $self->log->error( $msg );
+  die $msg;
+};
+
+method cmp ( Token $other! ){
+  return undef unless $self->set eq $other->set;
+  return $self->position <=> $other->position;
+};
+
+method _build_next () {
+  my( $position_n, $set ) = ( $self->position + 1, $self->set );
+  return undef if $position_n == $set->count;
+  return $set->tokens[$position_n];
+};
+
+method _set_next ( Token $next!, Token $old_next! ) {
+  my ( $position_n, $set ) = ( $self->position + 1, $self->set );
+  if( $position_n < $set->count ){
+    splice( @${ $self->set->tokens }, $position_n, 0, $next );
+    $next->next( $old_next );
+  }else{
+    push( @${ $self->set->tokens }, $next );
+  }
+  $next->prev($self);
+  return $next;
+};
+
+method _build_prev () {
+  my( $position_p, $set ) = ( $self->position - 1, $self->set );
+  return undef if $position_p < 0;
+  return $set->tokens[$position_p];
+};
+
+method _set_prev ( Token $prev!, Token $old_prev! ) {
+  my ( $position_p, $set ) = ( $self->position - 1, $self->set );
+  if( $position_p > 0 ){
+    splice( @${ $self->set->tokens }, $position_p, 0, $prev );
+    $old_prev->next($prev);
+  }else{
+    unshift( @${ $self->set->tokens }, $prev );
+  }
+  $prev->next($self);
+  return $prev;
+};
+
+__PACKAGE__->meta->make_immutable();
 
---
+++ Syntactic-Practice/lib/Syntactic/Practice/Lexer/TokenSet.pm
@@ -0,0 +1,96 @@
+package Syntactic::Practice::Grammar::TokenSet;
+
+=head1 NAME
+
+Syntactic::Practice::Grammar::TokenSet - A set of Tokens
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+use Moose;
+use namespace::autoclean;
+use MooseX::Method::Signatures;
+
+has tokens => ( is      => 'rw',
+                isa     => 'ArrayRef[Token]',
+                lazy    => 1,
+                builder => '_build_tokens', );
+
+has first => ( is      => 'rw',
+               isa     => 'Maybe[Token]',
+               lazy    => 1,
+               builder => '_build_first',
+               trigger => \&_set_first, );
+
+has last => ( is      => 'rw',
+              isa     => 'Maybe[Token]',
+              lazy    => 1,
+              builder => '_build_last',
+              trigger => \&_set_last, );
+
+sub _build_tokens {
+  return [];
+}
+
+sub copy {
+  my ( $self, %attr ) = @_;
+  %attr = ( %$self, %attr );
+  map{ delete $attr{$_} } ( qw( tokens first last ) );
+
+  my $copy = $self->new( %attr );
+  return $copy unless $self->count;
+
+  $copy->tokens( [ map { $_->copy( set => $copy ) } @{ $self->tokens } ] );
+  return $copy;
+}
+
+method _build_last { $self->tokens ? $self->tokens->[-1] : undef };
+
+method _set_last ( Token $last!, Maybe[Token] $old_last! ) {
+  return $last unless $old_last;
+  my $cursor = $last;
+  while ( $cursor->prev ) { $cursor = $cursor->prev }
+  $old_last->next( $cursor );
+  return $last;
+};
+
+method append ( 'TokenSet|Token' $more! ) {
+  if ( $more->isa( 'Syntactic::Practice::Grammar::TokenSet' ) ) {
+    $self->last( $more->copy->last );
+  } elsif ( $more->isa( 'Syntactic::Practice::Grammar::Token' ) ) {
+    $self->last( $more->copy( set => $self ) );
+  }
+  return $self;
+};
+
+method _build_first { $self->tokens ? $self->tokens->[0] : undef }
+
+method _set_first ( Token $first!, Maybe[Token] $old_first! ) {
+  $self->first->next( $old_first );
+  return $first unless $old_first;
+  my $cursor = $first;
+  while ( $cursor->next ) { $cursor = $cursor->next }
+  $cursor->next( $old_first );
+  return $first;
+};
+
+method prepend ( 'TokenSet|Token' $more! ) {
+  if ( $more->isa( 'Syntactic::Practice::Grammar::TokenSet' ) ) {
+    $self->first( $more->copy->first );
+  } elsif ( $more->isa( 'Syntactic::Practice::Grammar::Token' ) ) {
+    $self->first( $more->copy( set => $self ) );
+  }
+  return $self;
+};
+
+method count () {
+  $self->tokens ? scalar @{ $self->tokens } : 0;
+};
+
+__PACKAGE__->meta->make_immutable();
+

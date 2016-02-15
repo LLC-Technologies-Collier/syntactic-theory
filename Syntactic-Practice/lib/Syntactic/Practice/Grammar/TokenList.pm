082af966c3670a6849874ecc596fb00b5cffea1a
C.J. Collier <cjcollier@linuxfoundation.org>
2016-02-14 21:47:52 -0800

    expanded the STORE method with some input validation and preparation for GC and linked list insertion

--- Syntactic-Practice/lib/Syntactic/Practice/Grammar/TokenList.pm
+++ Syntactic-Practice/lib/Syntactic/Practice/Grammar/TokenList.pm
@@ -24,8 +24,26 @@ sub TIEARRAY {
   bless { set => $set, array => [] }, $class;
 }
 
-sub FETCH     { $_[0]->{array}->[ $_[1] ] }
-sub STORE     { $_[0]->{array}->[ $_[1] ] = $_[2] }
+sub FETCH { $_[0]->{array}->[ $_[1] ] }
+
+my $number_rx = qr/^\d+$/;
+
+sub STORE {
+  my ( $self, $i, $token ) =
+    validate_pos( @_, 1,
+                  { regex => $number_rx },
+                  { isa   => 'Syntactic::Practice::Grammar::Token' } );
+
+  die 'This token does not belong to the correct set'
+    unless $self->{set} <=> $token->set;
+
+  my $prev = $i == 0 ? undef : $self->{array}->[ $i - 1 ];
+  my $next = $i == $#{ $self->{array} } ? undef : $self->{array}->[ $i + 1 ];
+
+  my $old = $self->{array}->[$i];
+
+  $self->{array}->[$i] = $token;
+}
 sub FETCHSIZE { scalar @{ $_[0]->{array} } }
 sub STORESIZE { $#{ $_[0]->{array} } = $_[1] - 1 }
 sub EXTEND    { $_[0]->STORESIZE( $_[1] ) }
